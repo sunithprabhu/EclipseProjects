aa82d6bd1b7649920173bcb3957f2fb6e03321ab
sunithprabhu <sunithprabhu@gmail.com>
2020-05-14 12:06:20 +0530

    Next Day of Jenkins

---
+++ LambdaExpressions/src/Demo.java
@@ -0,0 +1,21 @@
+
+interface First {
+	
+	public default void jesus()
+	{
+		System.out.println("Thank you jesus");
+	}
+
+}
+interface Second{
+	public default void yehova()
+	{
+		System.out.println("Thank you my god ");
+	}
+}
+
+public class Demo implements First,Second{
+	
+	
+	
+}
